bd2776f55a12871a5c6cf88c8a4d1857d3cafcf0
Avruschenko Dmytro <trippyfren@gmail.com>
2023-05-11 16:23:03 +0300

    Create simple script for starting telegram bot

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 import requests
 from datetime import datetime
+import telebot
+import secret_key
 
 
 def get_data():
     req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
     response = req.json()
-    print(response)
+    # print(response)
     sell_price = response["btc_usd"]["sell"]
-    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}$")
+    return f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}$"
 
+
+def telegram_bot(token):
+    bot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands=["start"])
+    def start_message(message):
+        bot.send_message(message.chat.id, "Hello friend! Write the 'price' to find out the cost of BTC")
+
+    @bot.message_handler(content_types=["text"])
+    def send_text(message):
+        if message.text.lower() == "price":
+            try:
+                bot.send_message(
+                    message.chat.id,
+                    get_data()
+                )
+            except Exception as ex:
+                print(ex)
+                bot.send_message(
+                    message.chat.id,
+                    "SMTH went wrong"
+                )
+        else:
+            bot.send_message(
+                message.chat.id,
+                "Not an option"
+            )
+
+
+    bot.polling()
 
 if __name__ == "__main__":
-    get_data()
+    # get_data()
+    telegram_bot(secret_key.TOKEN_TELGRAM_BOT)
